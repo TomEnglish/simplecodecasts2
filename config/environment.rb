30f177b0c44c58588881fba83353a2a5ab6a799d
TomEnglish <t.elliott.english@gmail.com>
2016-10-25 17:35:54 +0000

    trying a config change

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,31 @@ Rails.application.initialize!
 
 
 
-config.action_mailer.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :domain => 'heroku.com',
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+Rails.application.configure do
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.js_compressor = :uglifier
+  config.assets.compile = false
+  config.assets.digest = true
+  config.log_level = :debug
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {  host: "https://personal-blog22.herokuapp.com " }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
